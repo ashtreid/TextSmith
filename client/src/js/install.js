4b22068aa1c351d73ff7d59d3c936da2fd7f54fb
Ash Reid <ashley.reid013@gmail.com>
2023-07-07 22:17:27 -0600

    adds readme

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,19 +2,16 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log("before install prompt");
     window.deferredPrompt = event;
 
     butInstall.classList.toggle('hidden', false);
 });
 
 butInstall.addEventListener('click', async () => { 
-    console.log("clicky click");
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
         return;
     }
-    console.log("prompt event", promptEvent);
     promptEvent.prompt();
     window.deferredPrompt = null;
 
@@ -22,7 +19,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 window.addEventListener('appinstalled', (event) => {
-    console.log("app installed")
     window.deferredPrompt = null;
 });
 
